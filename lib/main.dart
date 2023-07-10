e6eb076e0f26b27c971fca7542a6ab017590700d
ehsancodes <abu.md.ehsan@gmail.com>
2023-07-10 23:42:53 +0600

    live9

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Future<List<Recipe>> _getRecipes() async {
+
+    var data = await http.get("http://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2");
+
+    var jsonData = json.decode(data.body);
+
+    List<Recipe> users = [];
+
+    for(var r in jsonData){
+
+      Recipe recipe = Recipes(r["title"], r["descriptions"], r["Ingridents"]);
+
+      users.add(recipe);
+
+    }
+
+    print(_getRecipes());
+
+    return users;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: _getRecipes(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            print(snapshot.data);
+            if(snapshot.data == null){
+              return Container(
+                  child: Center(
+                      child: Text("Loading...")
+                  )
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          snapshot.data[index].picture
+                      ),
+                    ),
+                    title: Text(snapshot.data[index].name),
+                    subtitle: Text(snapshot.data[index].email),
+                    onTap: (){
+
+                      Navigator.push(context,
+                          new MaterialPageRoute(builder: (context) => DetailPage(snapshot.data[index]))
+                      );
+
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+
+  final Recipe recipe;
+
+  DetailPage(this.recipe);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(recipe.name),
+        )
+    );
+  }
+}
+
+
+class Recipe {
+  final String Title;
+  final String description;
+  final String Ingredients;
+
+
+  Recipe(this.Title, this.description, this.Ingredients);
+
+
+
+}
